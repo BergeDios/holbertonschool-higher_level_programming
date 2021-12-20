7f0a8784e1bb60200027a47effc38bc92792f154
BergeDios <santiago.goyret@gmail.com>
2021-12-20 09:06:04 -0800

    task 10 working locally

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a single linked list has a cycle in it
+ * @list: linked list
+ * Return: 0 if no cycle and 1 if it has one
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *ahead = list, *prev = list;
+
+	if (!list)
+		return (0);
+	while (ahead->next->next != NULL)
+	{
+		ahead = ahead->next->next;
+		prev = prev->next;
+
+		if (ahead == prev)
+			return (1);
+	}
+	return (0);
+}
