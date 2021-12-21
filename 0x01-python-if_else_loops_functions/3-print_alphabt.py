c803698ff6b749c3c0612863339e060ef1a8a944
BergeDios <santiago.goyret@gmail.com>
2021-12-21 07:31:21 -0800

    fixed pepochos

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for c in range(97, 123):
-    if c in [101,113]:
+    if c in [101, 113]:
         continue
     else:
-        print(chr(c),end='')
+        print(chr(c), end='')
