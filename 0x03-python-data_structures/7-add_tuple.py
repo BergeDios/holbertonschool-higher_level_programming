3208f254de891a1954335a397441082f78d9e530
BergeDios <santiago.goyret@gmail.com>
2022-01-03 09:51:58 -0800

    task 7 add blank line for pep8

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def add_tuple(tuple_a=(), tuple_b=()):
     lengtha = len(tuple_a)
     lengthb = len(tuple_b)
@@ -22,5 +23,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     else:
         b1 = tuple_b[0]
         b2 = tuple_b[1]
+
     new_tuple = (a1 + b1, a2 + b2)
+
     return (new_tuple)
