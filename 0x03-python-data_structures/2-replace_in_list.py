59462966b0136a6d8eab4f80d86c618cd5613563
BergeDios <santiago.goyret@gmail.com>
2022-01-03 08:07:07 -0800

    task until 2 working locally

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,6 @@
+def replace_in_list(my_list, idx, element):
+    length = len(my_list) - 1
+    if idx > length or idx < 0:
+        return (my_list)
+    my_list[idx] = element
+    return (my_list)
