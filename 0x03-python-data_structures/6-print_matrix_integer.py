67a5afd49e77fc04f989c74a30b645282df11f4b
BergeDios <santiago.goyret@gmail.com>
2022-01-03 09:39:41 -0800

    task 6 locally

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    length = len(matrix) - 1
+    if not matrix:
+        print()
+    for row in matrix:
+        count = 0
+        for i in row:
+            if count != 0:
+                print(' ', end='')
+            print("{:d}".format(i), end='')
+            count += 1
+        print()
