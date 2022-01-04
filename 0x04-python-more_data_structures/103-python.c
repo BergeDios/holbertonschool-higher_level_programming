82f451501629ffb9213dee2b91029afae2c21fc3
BergeDios <santiago.goyret@gmail.com>
2022-01-04 09:58:39 -0800

    103 template

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <Python.h>
+/**
+ * print_python_list - prints python list
+ * @p: pointer to sabemos que
+ * Return: nothing
+ */
+void print_python_list(PyObject *p)
+{
+	void(PyObject);
+
+	ni idea que hace todavia
+}
