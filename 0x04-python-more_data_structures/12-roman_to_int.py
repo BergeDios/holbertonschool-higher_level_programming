ee27b50185c7dedf6c83f6097ce0c46359b489c4
BergeDios <santiago.goyret@gmail.com>
2022-01-04 09:05:08 -0800

    task 12 working locally with pep8

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,13 +7,13 @@ def roman_to_int(roman_string):
     if roman_string == "":
         return 0
     num = 0
-    num_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    num_dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for i, j in zip(roman_string, roman_string[1:]):
-        if i not in num_dict.keys():
+        if i not in num_dic.keys():
             return 0
-        elif num_dict[i] >= num_dict[j]:
-            num += num_dict[i]
+        elif num_dic[i] >= num_dic[j]:
+            num += num_dic[i]
         else:
-            num -= num_dict[i]
-    num += num_dict[roman_string[-1]]
+            num -= num_dic[i]
+    num += num_dic[roman_string[-1]]
     return num
