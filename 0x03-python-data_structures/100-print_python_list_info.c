d471665000db4b7b9018e20c74ca739f3886b64d
BergeDios <santiago.goyret@gmail.com>
2022-01-04 10:03:19 -0800

    task 100 template

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <Python.h>
+/**
+ * print_python_list_info - prints python list info
+ * @p: somethin
+ * Return: void
+ */
+void print_python_list_info(PyObject *p)
+{
+	void(p);
+}
