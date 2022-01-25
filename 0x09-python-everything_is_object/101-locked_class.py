60198827bf2eb19af20cdea8ca7e882c2a14f8cc
BergeDios <santiago.goyret@gmail.com>
2022-01-25 11:13:33 -0800

    todays task

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+
+Module 101-locked_class
+
+"""
+
+
+class LockedClass:
+    """defining locked class to only recieve attr first_name"""
+
+    __slots__ = ['first_name']
+
+    def init(self):
+        """initialization"""
+
+        pass
