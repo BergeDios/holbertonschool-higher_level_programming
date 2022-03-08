11a47302c10514137296f7b7bdd6f1bcfdba767c
BergeDios <santiago.goyret@gmail.com>
2022-03-08 10:58:02 -0800

    task 100 try

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,4 +3,4 @@
 
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE hbtn_0c_0;
-ALTER TABLE first_table	CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
