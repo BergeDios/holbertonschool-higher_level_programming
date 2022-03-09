b1a4873ac35f2a17159f9ce457893d9439ba60aa
BergeDios <santiago.goyret@gmail.com>
2022-03-08 18:22:57 -0800

    task 102 and 103

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,3 @@
+-- script displaying top 3 citys temps in july and august
+
+SELECT city, AVG(value) AS 'avg_temp' FROM temperatures WHERE `month`=7 OR `month`=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
