cb27b7b53ed6b7cbdac87f3f42a67c7bb67613b7
BergeDios <santiago.goyret@gmail.com>
2022-03-09 19:04:18 -0800

    fix maldito join

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,9 +6,9 @@
 -- can use only one SELECT statement
 -- the database name will be passed as an argument of the mysql command
 
-    SELECT tv_genres.name AS genre, COUNT(*) AS 'number_shows'
-      FROM tv_genres
-INNER JOIN tv_show_genres
+    SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS 'number_shows'
+      FROM tv_show_genres
+	  JOIN tv_genres
         ON tv_genres.id = tv_show_genres.genre_id
-  GROUP BY genre
+  GROUP BY tv_show_genres.genre_id
   ORDER BY number_shows DESC;
