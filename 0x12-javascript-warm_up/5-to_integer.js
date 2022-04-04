a28509861911212decd6ccde97a2fefccf4a423f
BergeDios <santiago.goyret@gmail.com>
2022-04-04 10:26:07 -0700

    task 5 test 100

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const pnum = parseInt(args[0]);
-if (Number.isNaN(pnum)) {
+if (isNaN(pnum)) {
   console.log('Not a number');
 } else {
-  console.log('My Number: '.concat(pnum));
+  console.log('My Number: ' + pnum);
 }
