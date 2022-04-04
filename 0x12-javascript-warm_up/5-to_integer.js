24bd85cf2c54c0e365fe3192d01eaa046382ba4e
BergeDios <santiago.goyret@gmail.com>
2022-04-04 10:21:27 -0700

    task 5 with number.isnan

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const args = process.argv;
-const pnum = parseInt(args[2]);
-if (isNaN(pnum)) {
+const args = process.argv.slice(2);
+const pnum = parseInt(args[0]);
+if (Number.isNaN(pnum)) {
   console.log('Not a number');
 } else {
   console.log('My Number: '.concat(pnum));
