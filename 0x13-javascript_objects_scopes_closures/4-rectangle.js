0869c894935be4ecc19cd8106938d738df26708b
BergeDios <santiago.goyret@gmail.com>
2022-04-05 07:36:54 -0700

    up to 8 done locally

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,27 +1,30 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor (w, h){
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    };
-  };
+    }
+  }
+
   print () {
     let string = '';
     for (let i = 0; i < this.width; i++) {
       string = string + 'X';
-    };
+    }
     for (let j = 0; j < this.height; j++) {
       console.log(string);
-    };
-  };
+    }
+  }
+
   rotate () {
     const aux = this.width;
     this.width = this.height;
     this.height = aux;
-  };
+  }
+
   double () {
     this.width = this.width * 2;
     this.height = this.height * 2;
-  };
+  }
 };
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,10 +3,19 @@ const Rectangle = require('./4-rectangle');
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-  };
-  charPrint(c) {
+  }
+
+  charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
-      
+      let string = '';
+      for (let i = 0; i < this.width; i++) {
+        string = string + c;
+      }
+      for (let j = 0; j < this.height; j++) {
+        console.log(string);
+      }
+    }
+  }
 };
