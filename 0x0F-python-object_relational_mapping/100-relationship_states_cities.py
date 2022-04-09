0e8ba18a805c17eb60679c54b5614709fa47f580
BergeDios <santiago.goyret@gmail.com>
2022-04-09 14:04:43 -0700

    task 100 try 1000

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -2,10 +2,10 @@
 """Start link class to table in database
 """
 import sys
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
     state_obj = State(name="California")
     city_obj = City(name="San Francisco")
