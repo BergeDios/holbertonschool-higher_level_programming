5162669abd0e8009b1b326520d5699b2fec7b788
BergeDios <santiago.goyret@gmail.com>
2022-04-09 13:07:50 -0700

    task 2 fix try

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
                          db=argv[3],
                          port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE NAME LIKE '{}'"
+    cur.execute("SELECT * FROM states WHERE NAME LIKE '{:s}'"
                 .format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
