6cdefd84e263403b3a4fea6fd0cb11369b75307d
BergeDios <santiago.goyret@gmail.com>
2022-04-24 22:24:07 +0000

    task 9 syntax dislexya

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,6 +10,6 @@ import sys
 if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
-    url = 'https://api.github.com/user{}'.format(user)
+    url = 'https://api.github.com/user/{}'.format(user)
     r = requests.get(url, auth=requests.auth.HTTPBasicAuth(user, password))
     print(r.json().get('id'))
