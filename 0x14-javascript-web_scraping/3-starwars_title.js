079bc4abd82efd0ba89e2f61ba4eb5f913cb080c
BergeDios <santiago.goyret@gmail.com>
2022-05-17 20:00:55 +0000

    task 3 fix ss

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // Script that uses the Star Wars api to request for pages with movie id
 const axios = require('axios').default;
-const movie_id = process.argv[2];
-axios.get('http://swapi-api.hbtn.io/api/films/' + movie_id)
+const movieId = process.argv[2];
+axios.get('http://swapi-api.hbtn.io/api/films/' + movieId)
   .then((response) => {
     console.log(response.data.title);
   })
-  .catch((error) =>{
+  .catch((error) => {
     console.log(error);
   })
   .then(() => {
