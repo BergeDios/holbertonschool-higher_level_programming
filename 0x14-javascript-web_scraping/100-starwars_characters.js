18a062211cb52b4153d6f5733bb39d73a27ea493
BergeDios <santiago.goyret@gmail.com>
2022-05-17 21:24:42 +0000

    task 100 and 101 nice comments

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,11 +2,15 @@
 // Script that prints every character for a star wars movie of given id
 
 const axios = require('axios').default;
+// get request to star wars api
 axios.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2])
   .then((response) => {
+    // with response save character list in variable
     const characters = response.data.characters;
+    // iterate over it creating new requests to get name
     for (let i = 0; i < characters.length; i++) {
       const axioschar = require('axios').default;
+      // new axios to get request with each character
       axioschar.get(characters[i])
         .then((resp) => {
           console.log(resp.data.name);
