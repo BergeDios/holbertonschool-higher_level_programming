6d0a0ce1e80eacc9742fcbe8a06eaa5923142ea7
BergeDios <santiago.goyret@gmail.com>
2022-05-17 15:59:36 +0000

    task 0 fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 // Script that reads and prints the content of a file given as parameter
-const file = process.argv[2];
 const fs = require('fs');
-fs.readFile(file, 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
-  }
-  else {
+  } else {
     console.log(data);
   }
 });
