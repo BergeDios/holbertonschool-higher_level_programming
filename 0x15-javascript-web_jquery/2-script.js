6c53eb4be1af923fe69d62fecba6aa8b27fed00e
BergeDios <santiago.goyret@gmail.com>
2022-05-26 11:34:41 -0300

    task 0 1 and 2

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+// change div on click to red using jquery
+$(function() {
+    $("#red_header").click(function() {
+        $("header").css('color', 'red');
+    });
+});
---
+++ 0x15-javascript-web_jquery/README.md
@@ -0,0 +1,1 @@
+# Directory for hoblerton proj0x15-javascript-web_jquery
